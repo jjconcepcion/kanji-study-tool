525cc8952fea7529d387e19cd86c346e00cb3ce0
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2018-10-27 16:02:57 -0700

    Update home

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  if (!req.session.visits) {
-    req.session.visits = 1;
-  } else {
-    req.session.visits += 1;
-  }
-
-  const username = req.user ? req.user.username : '';
-
   res.render('index', {
-    title: 'Express',
-    visits: req.session.visits,
+    title: 'Kanji Study Tool',
     loggedIn: req.user,
-    username,
   });
 });
 
