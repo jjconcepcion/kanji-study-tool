004b06d73d890997f2b2e497aac8de17c9429116
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2018-10-28 17:16:39 -0700

    Display kanji note text when logged in

--- routes/kanji.js
+++ routes/kanji.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Kanji = require('../models/kanji');
+const Note = require('../models/note');
 
 const router = express.Router();
 
@@ -10,14 +11,26 @@ router.get('/:character', (req, res) => {
   }
   const { character } = req.params;
   const { search } = req.session
+  const userId = (req.user) ? req.user.id : undefined;
   let previouslySearched = false
   // check if requested character saved from previous search
   if (search && search.results) {
     const kanjiData = search.results.filter(row => row.kanji === character);
-    if (kanjiData) {
+    if (kanjiData[0]) {
       previouslySearched = true;
       renderOptions.kanjiData = kanjiData[0];
-      res.render('kanji', renderOptions);
+      // get kanji note if logged in
+      if (!kanjiData[0].note && userId) {
+        Note.getText(kanjiData[0].rtk_no, userId)
+          .then(results => {
+            const note = (results.rows[0])? results.rows[0].text : undefined;
+            renderOptions.kanjiData.note = note;
+            res.render('kanji', renderOptions)
+          })
+          .catch(err => console.log(err));
+      } else {
+        res.render('kanji', renderOptions);
+      }
     }
   }
   if (!previouslySearched) {
@@ -26,11 +39,24 @@ router.get('/:character', (req, res) => {
         const { rowCount, rows } = results;
         if (rowCount == 1) {
           renderOptions.kanjiData = rows[0];
-          res.render('kanji', renderOptions);
+          return renderOptions.kanjiData.rtk_no;
         } else {
           renderOptions.title = 'Not Found';
           renderOptions.message = 'The requested kanji was not found';
           res.status(404).render('404', renderOptions)
+        }
+      })
+      .then(kanjiId => {
+        if (userId) {
+          Note.getText(kanjiId, userId)
+            .then(results => {
+              const note = (results.rows[0])? results.rows[0].text : undefined;
+              renderOptions.kanjiData.note = note;
+              res.render('kanji', renderOptions)
+            })
+            .catch(err => console.log(err));
+        } else {
+          res.render('kanji', renderOptions)
         }
       })
       .catch(err => console.log(err));
